5aec363591bd4d91f57bc30a974bfc7b8cf2669f
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-10-30 13:04:16 -0600

    init

--- provision/main.tf
+++ provision/main.tf
@@ -17,7 +17,7 @@ resource "google_dns_record_set" "artifactory" {
   managed_zone = data.google_dns_managed_zone.harness.name
   type         = "CNAME"
   ttl          = 300
-  rrdatas      = ["artifactory.citibanamex.${data.google_dns_managed_zone.harness.dns_name}."]
+  rrdatas      = ["artifactory.citibanamex.${data.google_dns_managed_zone.harness.dns_name}"]
 }
 
 resource "google_dns_record_set" "sonarqube" {
@@ -25,5 +25,5 @@ resource "google_dns_record_set" "sonarqube" {
   managed_zone = data.google_dns_managed_zone.harness.name
   type         = "CNAME"
   ttl          = 300
-  rrdatas      = ["sonarqube.citibanamex.${data.google_dns_managed_zone.harness.dns_name}."]
+  rrdatas      = ["sonarqube.citibanamex.${data.google_dns_managed_zone.harness.dns_name}"]
 }
