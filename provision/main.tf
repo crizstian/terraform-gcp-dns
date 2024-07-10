9697cbfc0a651b4b37a5dbfe2638bebc37e67801
Cristian Ramirez <cristiano.rosetti@gmail.com>
2024-07-09 20:33:10 -0600

    Update main.tf

--- provision/main.tf
+++ provision/main.tf
@@ -12,38 +12,3 @@ resource "google_dns_record_set" "dns" {
   rrdatas = [var.gke_ingress_ip]
 }
 
-resource "google_dns_record_set" "artifactory" {
-  count = 0
-  name         = "artifactory.selatam.${data.google_dns_managed_zone.harness.dns_name}"
-  managed_zone = data.google_dns_managed_zone.harness.name
-  type         = "CNAME"
-  ttl          = 300
-  rrdatas      = ["selatam.${data.google_dns_managed_zone.harness.dns_name}"]
-}
-
-resource "google_dns_record_set" "sonarqube" {
-  count = 0
-  name         = "sonarqube.selatam.${data.google_dns_managed_zone.harness.dns_name}"
-  managed_zone = data.google_dns_managed_zone.harness.name
-  type         = "CNAME"
-  ttl          = 300
-  rrdatas      = ["selatam.${data.google_dns_managed_zone.harness.dns_name}"]
-}
-
-resource "google_dns_record_set" "prometheus" {
-  count = 0
-  name         = "prometheus.selatam.${data.google_dns_managed_zone.harness.dns_name}"
-  managed_zone = data.google_dns_managed_zone.harness.name
-  type         = "CNAME"
-  ttl          = 300
-  rrdatas      = ["selatam.${data.google_dns_managed_zone.harness.dns_name}"]
-}
-
-resource "google_dns_record_set" "grafana" {
-  count = 0
-  name         = "grafana.selatam.${data.google_dns_managed_zone.harness.dns_name}"
-  managed_zone = data.google_dns_managed_zone.harness.name
-  type         = "CNAME"
-  ttl          = 300
-  rrdatas      = ["selatam.${data.google_dns_managed_zone.harness.dns_name}"]
-}
